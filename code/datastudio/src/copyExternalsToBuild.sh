4ccdb377a76d114e624915e6a726dd4fc85e7f44
name <author>
2021-12-16 19:34:36 +0800

    modified build pre-scripts for bulid linux development environment
    Match-id-44ad2fa9ba1df234f7ced3b7ff6a55698b25f2b5

--- code/datastudio/src/copyExternalsToBuild.sh
+++ code/datastudio/src/copyExternalsToBuild.sh
@@ -8,10 +8,11 @@ rm -rf ../docs
 rm -rf $DEST/../build
 rm -rf $DEST/db_assistant
 
-rm -rf $DEST\com.huawei.mppdbide.presentation\src-test\*.jar
-cp $SOURCE/buildtools/DS_CommonTestJars $DEST/com.huawei.mppdbide.presentation/src-test/.
-cp $SOURCE%/buildtools/DS_CommonTestJars/MockRunner $DEST/com.huawei.mppdbide.presentation/src-test/.
-cp $SOURCE/buildtools/DS_CommonTestJars/easyMock $DEST/com.huawei.mppdbide.presentation/src-test/.
+rm -rf $DEST/com.huawei.mppdbide.presentation/src-test/com/huawei/mppdbide/test-libs
+mkdir $DEST/com.huawei.mppdbide.presentation/src-test/com/huawei/mppdbide/test-libs
+cp -r $SOURCE/buildtools/DS_CommonTestJars $DEST/com.huawei.mppdbide.presentation/src-test/.
+cp -r $SOURCE/buildtools/DS_CommonTestJars/MockRunner $DEST/com.huawei.mppdbide.presentation/src-test/.
+cp -r $SOURCE/buildtools/DS_CommonTestJars/easyMock $DEST/com.huawei.mppdbide.presentation/src-test/.
 
 #::1. Copy the external files from platform, 3rd Src to respective src folders
 cp $SOURCE/3rd_src/apache-commons-collections/commons-collections4-4.4.jar $DEST/Common-collection/.
@@ -25,23 +26,19 @@ cp $SOURCE/3rd_src/commons-csv/commons-csv-1.7.jar $DEST/commons-csv/.
 cp $SOURCE/3rd_src/jsql-parser/jsqlparser-3.2.jar $DEST/JSQLParser/.
 cp $SOURCE/3rd_src/jquery/jquery.min.js $DEST/../db_assistant/js/.
 cp $SOURCE/3rd_src/antlr/antlr4-runtime-4.7.2.jar $DEST/com.huawei.mppdbide.parser/.
-cp $SOURCE/3rd_src/antlr/antlr4-runtime-4.7.2.jar $DEST/com.huawei.mppdbide.staticcheck/.
 cp $SOURCE/3rd_src/apache-log4j/log4j-api-2.13.2.jar $DEST/com.huawei.mppdbide.utils/.
 cp $SOURCE/3rd_src/apache-log4j/log4j-core-2.13.2.jar $DEST/com.huawei.mppdbide.utils/.
 cp $SOURCE/3rd_src/gson/gson-2.8.6.jar $DEST/com.huawei.mppdbide.bl/.
 cp $SOURCE/3rd_src/gson/gson-2.8.6.jar $DEST/com.huawei.mppdbide.explainplan/.
 cp $SOURCE/3rd_src/gson/gson-2.8.6.jar $DEST/com.huawei.mppdbide.view/.
-cp $SOURCE/3rd_src/gson/gson-2.8.6.jar $DEST/com.huawei.mppdbide.staticcheck/.
 cp $SOURCE/3rd_src/google/guice-4.2.0.jar $DEST/com.huawei.mppdbide.view/.
 cp $SOURCE/3rd_src/google/guava-30.1.1-jre.jar $DEST/com.huawei.mppdbide.view/.
 cp $SOURCE/platform/Gauss200/gs_jdbc/gsjdbc4.jar $DEST/com.huawei.dbdriver.jdbc.gauss/.
 
 mkdir $DEST/com.huawei.mppdbide.repository/tools
 mkdir $DEST/db_assistant
-mkdir $DEST/com.huawei.mppdbide.repository/tools/win/FileUtil
 
 cp -r $DEST/../db_assistant/ $DEST/db_assistant/
-cp -r $SOURCE/platform/FileUtil/* $DEST/com.huawei.mppdbide.repository/tools/win/FileUtil/.
 cp $SOURCE/code/datastudio/src/StartDataStudio.bat $DEST/com.huawei.mppdbide.repository/tools/.
 mkdir ../docs
 cp -r $SOURCE/information/datastudio/* ../docs/. 
